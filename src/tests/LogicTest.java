3598407ccad27800ec07437fc54c2b34f4ba7c30
Vladyslav Daliavskyi <mobivlad@outlook.com>
2020-05-16 13:08:20 +0300

    Added several unit tests

--- src/tests/LogicTest.java
+++ src/tests/LogicTest.java
@@ -1,11 +1,46 @@
 package tests;
 
+import jcode.AlgoFunc;
+import jcode.DecryptModel;
+import jcode.EncryptModel;
+import jcode.Matrix;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class LogicTest {
     @Test
-    public void f(){
-        Assert.assertEquals(1,1);
+    public void textLenEqualsStencilLen(){
+        try {
+            Matrix stencil = AlgoFunc.generateStencil(4,8);
+            String in = "Abcdefghijklmnopqrstuvwxyzabcdef";
+            String out = new DecryptModel().getAlgoRes(new EncryptModel().getAlgoRes(in,stencil,true),stencil,true);
+            Assert.assertEquals(in,out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void textLenNotEqualsStencilLen(){
+        try {
+            Matrix stencil = AlgoFunc.generateStencil(4,8);
+            String in = "Abcdefghijklmnopqrstuv";
+            String out = new DecryptModel().getAlgoRes(new EncryptModel().getAlgoRes(in,stencil,true),stencil,true);
+            Assert.assertFalse(out.indexOf(in)==-1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void textLenMultipleStencilLen(){
+        try {
+            Matrix stencil = AlgoFunc.generateStencil(4,8);
+            String in = "AbcdefghijklmnopqrstuvwxyzabcdeffedcbazyxwvutsrqponmlkjihgfedcbA";
+            String out = new DecryptModel().getAlgoRes(new EncryptModel().getAlgoRes(in,stencil,true),stencil,true);
+            Assert.assertEquals(in,out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
